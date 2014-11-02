87d625acde1a0a492bab0a028501a7d8ea3a6101
Inna Bauman <inna@cloudlock.com>
2014-11-02 14:46:50 +0200

    Show about text when not signed-in

--- fcs_body/lib/router.js
+++ fcs_body/lib/router.js
@@ -1,33 +1,54 @@
 Router.configure({
-    layoutTemplate: 'layout',
-    loadingTemplate: 'loading',
-    waitOn: function () {
-//        handle = Meteor.subscribeWithPagination('buildings', 100);
-//        return handle;
+        layoutTemplate: 'layout',
+        loadingTemplate: 'loading',
+
+        onBeforeAction: function () {
+            // all properties available in the route function
+            // are also available here such as this.params
+
+            if (!Meteor.userId()) {
+                // if the user is not logged in, render the Login template
+                this.render('About');
+            } else {
+                // otherwise don't hold up the rest of hooks or our route/action function
+                // from running
+                this.next();
+            }
+        }
     }
-});
+);
 Router.map(function () {
     this.route('glassDoor', {
         path: '/'
     });
 
-    this.route('buildingsView', {
+
+    Router.route('/buildings/:flr', function () {
+        this.subscribe('Buildings', this.params.flr).wait();
+
+        if (this.ready()) {
+            this.render('buildingsView');
+        } else {
+            this.render('Loading');
+        }
+    });
+
+//    this.route('buildingsView', {
 //        path: '/buildings/:address',
-        path: '/buildings'
+//        path: '/buildings'
 //        data: function () {
-            /*
-            var parts = address.split(".");
-            var suffix = parts[parts.length - 1];
-            if (suffix === "" || suffix.startsWith("l")) {
-                var level = address;
-                return Buildings.find({level: level});
-            }
-            else if (suffix.startsWith("b(")) {
-                return Buildings.findOne({address: address});
-            }
-            */
+    /*
+     var parts = address.split(".");
+     var suffix = parts[parts.length - 1];
+     if (suffix === "" || suffix.startsWith("l")) {
+     var level = address;
+     return Buildings.find({level: level});
+     }
+     else if (suffix.startsWith("b(")) {
+     return Buildings.findOne({address: address});
+     }
+     */
 //            return Buildings.find();
 //        }
-    });
+//    });
 });
-Router.onBeforeAction('loading');
