f2553c171530b4cd5b8b6671b68b0ee8b9662a16
Udi h Bauman <dibaunaumh@gmail.com>
2014-12-01 22:07:47 +0200

    another building-address test

--- fcs_aux/tests/unit/test_buildings_model.py
+++ fcs_aux/tests/unit/test_buildings_model.py
@@ -1,7 +1,19 @@
 from mies.buildings.model import build_bldg_address
 
 
-def test_build_bldg_address():
-    expected = "g-b(12,3)-l2-b(23,456)"
-    got = build_bldg_address("g-b(12,3)-l2", 23, 456)
+def test_build_ground_level_bldg_address():
+    flr = "g"
+    x = 123
+    y = 456
+    expected = "g-b(123,456)"
+    got = build_bldg_address(flr, x, y)
+    assert got == expected
+
+
+def test_build_upper_level_bldg_address():
+    flr = "g-b(1,2)-l0"
+    x = 123
+    y = 456
+    expected = "g-b(1,2)-l0-b(123,456)"
+    got = build_bldg_address(flr, x, y)
     assert got == expected
