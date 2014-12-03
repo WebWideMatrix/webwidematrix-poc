cf6138eb9920149721c55f3e3751ec49f5af7024
Udi h Bauman <dibaunaumh@gmail.com>
2014-12-03 23:30:38 +0200

    load data-pipes test

--- fcs_aux/tests/unit/test_data_pipes_model.py
+++ fcs_aux/tests/unit/test_data_pipes_model.py
@@ -1,11 +1,11 @@
-from mock import patch, MagicMock
+from mock import patch
 
 from mies.data_pipes.model import load_data_pipes
 
 
 @patch('mies.data_pipes.model.MongoClient')
 def test_load_data_pipes(mongo_client):
-    mongo_client.meteor = MagicMock()
-    load_data_pipes()
-    print mongo_client.meteor.mock_calls
-    assert 1 == 0
+    mc = mongo_client.return_value
+    data_pipe_spec = {'status': 'active', 'connectedBldg': {'$exists': True}}
+    for page in load_data_pipes():
+        mc.meteor.data_pipes.find.assert_called_once_with(data_pipe_spec, skip=0, limit=100)
