9edc577d8a737a64702d691d3b16868fbe325810
Udi h Bauman <dibaunaumh@gmail.com>
2014-12-10 12:58:51 +0200

    WIP support positioning a building in the next free spot

--- fcs_aux/mies/buildings/model.py
+++ fcs_aux/mies/buildings/model.py
@@ -9,13 +9,19 @@ from mies.mongoconfig import MONOGO_HOST, MONOGO_PORT
 from mies.buildings.constants import FLOOR_W, FLOOR_H, PROXIMITY
 
 
+class NoSpotLeft(Exception):
+    pass
+
+
 def build_bldg_address(flr, x, y):
     return "{flr}-b({x},{y})".format(flr=flr, x=x, y=y)
 
 
-def is_vacant(address):
+def is_vacant(address, db):
     # TODO implement using cache
-    return True
+    # TODO abstract the DB & inject it
+    bldg = db.buildings.find({"address": address})
+    return bldg is not None
 
 
 def _create_trials_state():
@@ -25,10 +31,21 @@ def _create_trials_state():
     return trials_state
 
 
-def find_spot(flr, state=None, near_x=None, near_y=None):
+def _get_next_free(flr, db):
+    for x in xrange(FLOOR_H):
+        for y in xrange(FLOOR_W):
+            if is_vacant(build_bldg_address(flr, x, y), db):
+                return x, y
+    raise NoSpotLeft()
+
+
+def find_spot(flr, state=None, position_hints=None, db=None):
     if state is None:
         state = _create_trials_state()
-    # generate a random address
+    # calculate the address according to the hints
+    near_x = position_hints.get('near_x')
+    near_y = position_hints.get('near_y')
+    next_free = position_hints.get('next_free') or False
     if near_x is not None and near_y is not None:
         state['near_lookups_count'] += 1
         # have we almost exhausted the near by spots?
@@ -39,21 +56,22 @@ def find_spot(flr, state=None, near_x=None, near_y=None):
                            near_x + state['proximity'])
         y = random.randint(near_y - state['proximity'],
                            near_y + state['proximity'])
+    elif next_free:
+        x, y = _get_next_free(flr, db)
     else:
         x = random.randint(0, FLOOR_W)
         y = random.randint(0, FLOOR_H)
     return build_bldg_address(flr, x, y), x, y
 
 
-def construct_bldg(flr, near_x, near_y, content_type, key, payload):
-    # TODO revise to add more hints than just near
+def construct_bldg(flr, content_type, key, payload, position_hints=None, db=None):
     x = 0
     y = 0
     address = None
     trials_state = _create_trials_state()
     while address is None:
-        address, x, y = find_spot(flr, trials_state, near_x, near_y)
-        if not is_vacant(address):
+        address, x, y = find_spot(flr, trials_state, position_hints, db)
+        if not is_vacant(address, db):
             address = None
 
     # TODO revise to avoid infinite loop if no spot is available
@@ -78,18 +96,17 @@ def construct_bldg(flr, near_x, near_y, content_type, key, payload):
 
 
 @app.task(ignore_results=True)
-def create_buildings(content_type, keys, payloads, flr, near_x=None, near_y=None,
-                     next_free=False):
+def create_buildings(content_type, keys, payloads, flr, position_hints=None):
     """
     Creates a batch of buildings.
     :param content_type: the content-type of the buildings
     :param keys: the list of keys for the buildings
     :param payloads: the list of payloads for the buildings
     :param flr: the target floor in which to create the buildings
-    :param near_x: optional x coordinate, near which the buildings will be created
-    :param near_y: optional y coordinate, near which the buildings will be created
-    :param next_free: optional hint to create the buildings in the next free place
-    (sequentially)
+    :param position_hints: dict of hints where to position the new buildings, such as:
+    * near_x: x coordinate, near which the buildings will be created
+    * near_y: y coordinate, near which the buildings will be created
+    * next_free: if True, create the buildings in the next free place (sequentially)
     :return: the addresses of the created buildings.
     """
     def _create_batch_of_buildings():
@@ -105,7 +122,7 @@ def create_buildings(content_type, keys, payloads, flr, near_x=None, near_y=None
     buildings = []
     count = 0
     for i, payload in enumerate(payloads):
-        bldg = construct_bldg(flr, near_x, near_y, content_type, keys[i], payload)
+        bldg = construct_bldg(flr, content_type, keys[i], payload, position_hints, db)
         buildings.append(bldg)
         create_buildings.append(bldg["address"])
         if len(buildings) == batch_size:
