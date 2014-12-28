f0190427b09423461b4848348f1ba3af0de4b5a4
Udi h Bauman <dibaunaumh@gmail.com>
2014-12-28 12:00:02 +0200

    Added zoom & pan

--- fcs_body/client/views/buildings/buildings_view.js
+++ fcs_body/client/views/buildings/buildings_view.js
@@ -7,19 +7,30 @@ Template.buildingsView.helpers({
 
 
 Template.buildingsGrid.rendered = function() {
-    var SQUARE_WIDTH = 10;
+    var WIDTH = 1200,
+        HEIGHT = 600,
+        SQUARE_WIDTH = 10;
+
 
     var self = this;
-	var zoom = d3.behavior.zoom()
-		.scaleExtent([0.5, 10])
-		.on("zoom", zoomed);
 
-	function zoomed() {
-		d3.select("#map_viewport").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-	}
 
-    var scale = d3.scale.linear().domain([0, 100*SQUARE_WIDTH]).range([0, 600]);
-    var svg = d3.select('body').select('#grid').call(zoom);
+    var svg = d3.select("#display").append("svg")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT)
+        .append("g")
+        .call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
+        .append("g");
+
+    svg.append("rect")
+        .attr("class", "overlay")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT);
+
+
+
+    var scale = d3.scale.linear().domain([0, 100*SQUARE_WIDTH]).range([0, HEIGHT]);
+
     if (!self.handle) {
         self.handle = Meteor.autorun(function () {
             var bldgs = Buildings.find();
@@ -40,12 +51,11 @@ Template.buildingsGrid.rendered = function() {
                     }
                 }
             }
-            console.log(bldgsDict);
-            console.log(squares);
-            d3.select('.bldgs').selectAll('rect').remove();
-            d3.select('.bldgs').selectAll('rect').data(squares)
-                .enter()
-                .append('rect')
+
+            svg.selectAll('rect').remove();
+            svg.selectAll('rect')
+                .data(squares)
+                .enter().append('rect')
                 .attr('x', function(d) {return scale(d.x * SQUARE_WIDTH)})
                 .attr('y', function(d) {return scale(d.y * SQUARE_WIDTH)})
                 .attr('width', scale(SQUARE_WIDTH))
@@ -54,9 +64,9 @@ Template.buildingsGrid.rendered = function() {
                     if (d.contentType)
                         return "black";
                     else
-                        return "grey";
+                        return "white";
                 })
-                .attr("stroke-width", 1)
+                .attr("stroke-width", 0.1)
                 .attr("fill", function(d) {
                     if (d.contentType)
                         return "blue";
@@ -65,5 +75,8 @@ Template.buildingsGrid.rendered = function() {
                 });
 
         });
+    }
+    function zoom() {
+        svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
     }
 };
