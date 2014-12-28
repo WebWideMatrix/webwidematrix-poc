c5eee920902e6f70b26baceea7d4a26c8250ff6f
Udi h Bauman <dibaunaumh@gmail.com>
2014-12-28 11:15:06 +0200

    Added moment.js

--- fcs_body/lib/util.js
+++ fcs_body/lib/util.js
@@ -10,19 +10,5 @@ todayDateForDisplay = function() {
 };
 
 formatDate = function(d) {
-    var MONTH_NAME = {
-        0: "Jan",
-        1: "Feb",
-        2: "Mar",
-        3: "Apr",
-        4: "May",
-        5: "Jun",
-        6: "Jul",
-        7: "Aug",
-        8: "Sep",
-        9: "Oct",
-        10: "Nov",
-        11: "Dec"
-    };
-    return d.getFullYear() + "-" + MONTH_NAME[d.getMonth()] + "-" + d.getDate();
+    return moment(d).format("YYYY-MMM-D");
 };
