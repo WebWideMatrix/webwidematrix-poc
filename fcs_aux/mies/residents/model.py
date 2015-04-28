4a51c36e1cda0b33be233886750c9a078552b300
Udi h Bauman <dibaunaumh@gmail.com>
2015-04-28 06:04:39 -0400

    refactoring

--- fcs_aux/mies/residents/model.py
+++ fcs_aux/mies/residents/model.py
@@ -1,5 +1,5 @@
-from mies.data_pipes.model import STATUS_ACTIVE
 from mies.mongoconfig import get_db
+from mies.residents.acting.flow import ActingBehavior
 
 
 def load_residents(criteria=None, limit=100):
@@ -16,7 +16,13 @@ def load_residents(criteria=None, limit=100):
     done = False
     while not done:
         results = db.residents.find(spec, limit=limit, skip=skip)
+        results = [Resident(r) for r in results]
         yield results
-        results = list(results)
         skip += len(results)
         done = len(results) < limit
+
+
+class Resident(dict, ActingBehavior):
+    def __init__(self, data, **kwargs):
+        super(Resident, self).__init__(**kwargs)
+        self.update(data)
--- fcs_aux/mies/residents/object.py
+++ fcs_aux/mies/residents/object.py
@@ -1,7 +1,3 @@
-from mies.residents.acting.flow import ActingBehavior
 
 
-class Resident(dict, ActingBehavior):
-    def __init__(self, data, **kwargs):
-        super(Resident, self).__init__(**kwargs)
-        self.update(data)
+
