056652c722dd298a3cc016560581c8013f0921a1
Udi h Bauman <dibaunaumh@gmail.com>
2015-06-08 14:01:40 -0400

    WIP fetch-article skeleton

--- fcs_aux/fconveys/actions/fetch_content/fetch_article.py
+++ fcs_aux/fconveys/actions/fetch_content/fetch_article.py
@@ -8,23 +8,39 @@ logging = get_task_logger(__name__)
 def fetch_article_action(input_payload):
     logging.info("Fetching article from social post")
     logging.info(input_payload)
-    # TODO implement & remove dummy result
-    url = "http://example.org/articles/some.html"
-    result_payloads = [
-        {
-            "content_type": "article-text",
-            "key": url,
-            "payload": {
-                "url": url,
-                "text": "This is the article text"
-            },
-            "placement_hints": {
-                "new_bldg": True,
-                "same_flr": False,
-                "flr_above": True,
-                "location_by_index": False,
-                "same_location": True,
+
+    result_payloads = []
+    for link in input_payload["urls"]:
+        url = link.expanded_url
+        display_url = link.display_url
+        shortened_url = link.url
+
+        # TODO perform a GET request to fetch the link file
+
+        # TODO save the result into a temp file
+
+        # TODO parse the file with textract
+
+        # TODO delete the file
+
+        # TODO append the parsed article text
+        result_payloads.append(
+            {
+                "content_type": "article-text",
+                "key": url,
+                "payload": {
+                    "url": url,
+                    "text": "This is the article text"
+                },
+                "placement_hints": {
+                    "new_bldg": True,
+                    "same_flr": False,
+                    "flr_above": True,
+                    "location_by_index": False,
+                    "same_location": True,
+                }
             }
-        }
-    ]
+        )
+
+    return result_payloads
 
