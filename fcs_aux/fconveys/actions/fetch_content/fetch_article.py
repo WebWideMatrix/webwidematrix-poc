7fa332f320abfb643c74f0431e355955520fb5ab
Udi h Bauman <dibaunaumh@gmail.com>
2015-06-09 11:15:05 -0400

    WIP fetch-article

--- fcs_aux/fconveys/actions/fetch_content/fetch_article.py
+++ fcs_aux/fconveys/actions/fetch_content/fetch_article.py
@@ -1,8 +1,27 @@
 from celery.utils.log import get_task_logger
+import os
+import requests
+from tempfile import NamedTemporaryFile
+import textract
 from mies.celery import app
 
 logging = get_task_logger(__name__)
 
+
+def download_file(url, chunk_size=1024):
+    f = NamedTemporaryFile(delete=False)
+    local_filename = f.name
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            # filter out keep-alive new chunks
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    return local_filename
+
+def delete_downloaded_file(file_name):
+     os.unlink(file_name)
 
 @app.task(name='fetch-article')
 def fetch_article_action(input_payload):
@@ -16,12 +35,19 @@ def fetch_article_action(input_payload):
         shortened_url = link.url
 
         # TODO perform a GET request to fetch the link file
-
-        # TODO save the result into a temp file
+        file_name = download_file(url)
+        logging.info("Downloaded article into temp file: {}".format(file_name))
 
         # TODO parse the file with textract
+        text = textract.process(file_name)
+        logging.info("Extracted article text ({} characters)".format(len(text)))
+        logging.info("T"*100)
+        logging.info(text)
+        logging.info("T"*100)
 
         # TODO delete the file
+        delete_downloaded_file(file_name)
+        logging.info("Deleted temp file: {}".format(file_name))
 
         # TODO append the parsed article text
         result_payloads.append(
@@ -30,7 +56,9 @@ def fetch_article_action(input_payload):
                 "key": url,
                 "payload": {
                     "url": url,
-                    "text": "This is the article text"
+                    "display_url": display_url,
+                    "shortened_url": shortened_url,
+                    "text": text
                 },
                 "placement_hints": {
                     "new_bldg": True,
