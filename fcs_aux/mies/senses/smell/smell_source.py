a1e137560fbb99021002372993234085360850ef
Udi h Bauman <dibaunaumh@gmail.com>
2015-06-12 14:52:26 -0400

    skeleton for smell source API

--- fcs_aux/mies/senses/smell/smell_source.py
+++ fcs_aux/mies/senses/smell/smell_source.py
@@ -1,1 +1,18 @@
-__author__ = 'udi'
+
+DEFAULT_SMELL_EXPIRY = 24 * 60 * 60     # 1 day in seconds
+
+
+def create_smell_source(address, strength, expiry=DEFAULT_SMELL_EXPIRY):
+    # TODO build key
+
+    # TODO store in redis
+
+    pass
+
+
+def update_smell_source(address, strength, expiry=None):
+    # TODO build key
+
+    # TODO update entry in redis
+
+    pass
