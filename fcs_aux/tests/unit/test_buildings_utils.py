4b68e4a1d708261c69162f07445ab7c75b7cf66c
Udi h Bauman <dibaunaumh@gmail.com>
2015-07-01 06:19:37 -0400

    unit-test

--- fcs_aux/tests/unit/test_buildings_utils.py
+++ fcs_aux/tests/unit/test_buildings_utils.py
@@ -1,6 +1,7 @@
+import pytest
 from mies.buildings.utils import (get_flr, get_bldg,
                                   get_containing_bldg_address,
-                                  extract_bldg_coordinates)
+                                  extract_bldg_coordinates, replace_bldg_coordinates)
 
 
 def test_get_flr():
@@ -29,3 +30,14 @@ def test_extract_bldg_coordinates():
     expected = (3, 4)
     got = extract_bldg_coordinates(addr)
     assert got == expected
+
+@pytest.mark.parametrize("addr, x, y, expected_result",
+    [
+        ("g", 12, 17, "g"),
+        ("g-b(1,2)", 12, 17, "g-b(12,17)"),
+        ("g-b(1,2)-l0", 12, 17, "g-b(12,17)-l0"),
+        ("g-b(1,2)-l2-b(3,4)", 12, 17, "g-b(1,2)-l2-b(12,17)"),
+        ("g-b(1,2)-l2-b(3,4)-l0", 12, 17, "g-b(1,2)-l2-b(12,17)-l0"),
+    ])
+def test_replace_bldg_coordinates(addr, x, y, expected_result):
+    assert replace_bldg_coordinates(addr, x, y) == expected_result
